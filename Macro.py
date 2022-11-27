20e9ef2e49074a882f273ccf71b5ecd6d7d4f7e3
KrazyManJ <KrazyManJ@gmail.com>
2022-11-27 23:57:58 +0100

    Added macro json exporting / importing

--- Macro.py
+++ Macro.py
@@ -1,33 +1,36 @@
 import abc
 import time
+import json
 from pynput import mouse
 from threading import Thread
 
-# Add pixel art to figma
-
 __MOUSE__ = mouse.Controller()
+
+
+class MacroEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        if type(o) is Macro:
+            return {k: v for k, v in o.__dict__.items() if not k.startswith("_")}
+        elif isinstance(o, MacroAction):
+            return {**o.__dict__, "action_type": type(o).__name__}
+        return super().default(o)
 
 
 class Macro:
 
     def __init__(self):
-        self.__actions__ = []
+        self.actions = []
         self.__is_run__ = False
 
     def run(self):
         if self.__is_run__: return
         self.__is_run__ = True
-        for action in self.__actions__: action.execute()
+        for action in self.actions: action.execute()
         self.__is_run__ = False
 
     def add_action(self, action):
-        self.__actions__.append(action)
-
-    def __getitem__(self, item):
-        return self.__actions__[item]
-
-    def __delitem__(self, key):
-        del self.__actions__[key]
+        self.actions.append(action)
 
     def run_as_thread(self):
         if self.__is_run__: return
@@ -35,8 +38,25 @@ class Macro:
         thr.start()
         return thr
 
+    def to_json(self, indent):
+        return json.dumps(self, cls=MacroEncoder, indent=4, sort_keys=True)
 
-class MouseAction(abc.ABC):
+    @staticmethod
+    def from_json(json_data) -> "Macro":
+
+        def hook(data: dict):
+            if "action_type" in data:
+                return eval(data["action_type"])(**{k: v for k, v in data.items() if k not in ["action_type"]})
+            elif list(data.keys()) == [k for k in Macro().__dict__.keys() if not k.startswith("_")]:
+                m = Macro(**{k: v for k, v in data.items() if k not in ["actions"]})
+                for action in data["actions"]:
+                    m.actions.append(action)
+                return m
+
+        return json.loads(json_data, object_hook=hook)
+
+
+class MacroAction(abc.ABC):
 
     def __init__(self, delay):
         self.delay = delay
@@ -46,7 +66,7 @@ class MouseAction(abc.ABC):
         time.sleep(self.delay / 1000)
 
 
-class MouseMove(MouseAction):
+class MouseMove(MacroAction):
 
     def __init__(self, position, x, y, delay):
         super().__init__(delay)
@@ -62,7 +82,7 @@ class MouseMove(MouseAction):
             __MOUSE__.position = self.x or __MOUSE__.position[0], self.y or __MOUSE__.position[1]
 
 
-class MouseClick(MouseAction):
+class MouseClick(MacroAction):
 
     def __init__(self, button, click_type, delay):
         super().__init__(delay)
@@ -76,7 +96,7 @@ class MouseClick(MouseAction):
             MAP[self.click_type](mouse.Button[self.button])
 
 
-class MouseScroll(MouseAction):
+class MouseScroll(MacroAction):
 
     def __init__(self, dx, dy, delay):
         super().__init__(delay)
